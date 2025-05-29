89f5599025935072981d07a39d2f3fe0b465a4f1
akshaypareek01 <akshay96102@gmail.com>
2025-05-29 12:32:25 +0530

    working

--- app/api/config/BaseUrl.jsx
+++ app/api/config/BaseUrl.jsx
@@ -1,2 +1,3 @@
-export const Base_url = 'https://vsc-insurance-app-backend.onrender.com/v1/';
-// export const Base_url = 'http://localhost:3002/v1/';
+// export const Base_url = 'https://vsc-insurance-app-backend.onrender.com/v1/';
+
+export const Base_url = 'https://addon-api.theodin.in/vsc/v1/';
